290b9c9c5d9b9c4d7ad25352b125c6f98be00423
abhinashak <abhinashak>
2010-11-17 13:41:44 +0000

    *** empty log message ***

---
+++ src/test/com/bizosys/hsearch/index/DocTeaserTest.java
@@ -0,0 +1,47 @@
+package com.bizosys.hsearch.index;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import com.bizosys.ferrari.TestFerrari;
+
+public class DocTeaserTest extends TestCase {
+
+	public static void main(String[] args) throws Exception {
+		DocTeaserTest t = new DocTeaserTest();
+        TestFerrari.testAll(t);
+	}
+	
+	public void testSerialize(String type, String state, 
+		String orgunit, String geohouse, Long created, Long modified, 
+		Long validTill, Boolean secuity, Boolean sentiment) throws Exception {
+
+		DocMeta meta = new DocMeta();
+		meta.docType = type;
+		meta.state = state;
+		meta.tenant = orgunit;
+		meta.geoHouse= geohouse;
+		meta.createdOn = new Date(created);
+		meta.modifiedOn = new Date(modified);
+		meta.validTill = new Date(validTill);
+		meta.securityHigh = secuity;
+		meta.sentimentPositive = sentiment;
+		
+		byte[] bytes = meta.toBytes();
+		DocMeta deserialized = new DocMeta(bytes);
+		
+		assertEquals(type, deserialized.docType);
+		assertEquals(state, deserialized.state);
+		assertEquals(orgunit, deserialized.tenant);
+		assertEquals(geohouse, deserialized.geoHouse);
+
+		assertEquals(created.longValue(), deserialized.createdOn.getTime());
+		assertEquals(modified.longValue(), deserialized.modifiedOn.getTime());
+		assertEquals(validTill.longValue(), deserialized.validTill.getTime());
+		
+		assertEquals(secuity.booleanValue(), deserialized.securityHigh);
+		assertEquals(sentiment.booleanValue(), deserialized.sentimentPositive);
+		
+	}
+}
